661cf34bd112d7405d441c58ff633a338b43d84d
darksmoke <dsmoke@ukr.net>
2021-06-29 22:24:20 +0300

    Update kvm_live_backup.sh

--- Linux/kvm_live_backup.sh
+++ Linux/kvm_live_backup.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# ver. 0.1
+#
 backup_dir=/KVM/backup
-keep_day=7
 # Список работающих VM
-vm_list=`virsh list | grep running | awk '{print $2}'`
+#vm_list=`virsh list | grep running | awk '{print $2}'`
 # Список VM, заданных вручную, через пробел
-#vm_list=(vm-1 vm-2)
+vm_list=(interovoshi_files_win2008r2)
 # Лог файл
 logfile="/var/log/kvmbackup.log"
+keep_days=10
 
 if [[ 1 -lt $(dpkg --list | grep -c pigz) ]]; then echo "Please, install pigz (apt install pigz)" && exit 0; fi
 
@@ -14,36 +17,60 @@ data=`date +%Y-%m-%d_%H-%M-%S`
 # Использовать это условие, если список VM задается вручную
 #for activevm in "${vm_list[@]}";
 # Использовать это условие, если список работающих VM берется автоматически
+i=0
+
 for activevm in $vm_list
-    do
-        mkdir -p $backup_dir/$activevm
-        echo "`date +"%Y-%m-%d_%H-%M-%S"` Start backup $activevm" >> $logfile
-        virsh dumpxml $activevm > $backup_dir/$activevm/$activevm-$data.xml
-        echo "`date +"%Y-%m-%d_%H-%M-%S"` Create snapshots $activevm" >> $logfile
-        disk_list=`virsh domblklist $activevm | grep vd | awk '{print $1}'`
-        disk_path=`virsh domblklist $activevm | grep vd | awk '{print $2}'`
-        virsh snapshot-create-as --domain $activevm snapshot --disk-only --atomic --quiesce --no-metadata
-        sleep 2
+do
+    mkdir -p $backup_dir/$activevm
+    echo "`date +"%Y-%m-%d_%H-%M-%S"` Start backup $activevm" >> $logfile
+    virsh dumpxml $activevm > $backup_dir/$activevm/$activevm-$data.xml
+    echo "`date +"%Y-%m-%d_%H-%M-%S"` Create snapshots $activevm" >> $logfile
+
+    disk_list=`virsh domblklist $activevm | grep vd | awk '{print $1}'`
+    disk_path=`virsh domblklist $activevm | grep vd | awk '{print $2}'`
+
+    virsh snapshot-create-as --domain $activevm snapshot --disk-only --atomic --quiesce --no-metadata
+
+    if [ $? -eq 0 ]; then
+
         for path in $disk_path
-            do
-                echo "`date +"%Y-%m-%d_%H-%M-%S"` Create backup $activevm $path" >> $logfile
-                filename=`basename $path`
-                pigz -c $path > $backup_dir/$activevm/$filename-$data.gz
-                sleep 2
-            done
+        do
+            echo "`date +"%Y-%m-%d_%H-%M-%S"` Create backup $activevm $path" >> $logfile
+            filename=`basename $path`
+            pigz -c $path > $backup_dir/$activevm/$filename-$data.gz
+            sleep 2
+        done
+
         for disk in $disk_list
-            do
-                snap_path=`virsh domblklist $activevm | grep $disk | awk '{print $2}'`
-                echo "`date +"%Y-%m-%d_%H-%M-%S"` Commit snapshot $activevm $snap_path" >> $logfile
-                virsh blockcommit $activevm $disk --active --verbose --pivot
-                sleep 2
+        do
+            snap_path=`virsh domblklist $activevm | grep $disk | awk '{print $2}'`
+            echo "`date +"%Y-%m-%d_%H-%M-%S"` Commit snapshot $activevm $snap_path" >> $logfile
+
+            virsh blockcommit $activevm $disk --active --verbose --pivot
+
+    	    if [ $? -eq 0 ]; then
                 echo "`date +"%Y-%m-%d_%H-%M-%S"` Delete snapshot $activevm $snap_path" >> $logfile
                 rm $snap_path
-            done
-        echo "`date +"%Y-%m-%d_%H-%M-%S"` End backup $activevm" >> $logfile
-    done
+                echo "`date +"%Y-%m-%d_%H-%M-%S"` End backup $activevm \n" >> $logfile
+            else
+              i=$(( i+1 ))
+              echo "`date +"%Y-%m-%d_%H-%M-%S"` PROBLEM 1! $activevm" >> $logfile
+            fi
 
-/usr/bin/find $backup_dir -type f -mtime +${keep_day} -exec rm -rf {} \;
+        done
+    else
+    	i=$(( i+1 ))
+    	echo "`date +"%Y-%m-%d_%H-%M-%S"` PROBLEM 2! $activevm $snap_path" >> $logfile
+    fi
+
+    if [ $i -gt 0 ]; then
+        echo "`date +"%Y-%m-%d_%H-%M-%S"` COUNT PROBLEMS = $i \n" >> $logfile
+    fi
+done
+
+if [ $i -eq 0 ]; then
+    /usr/bin/find $backup_dir -type f -mtime +${keep_days} -exec rm -rf {} \;
+fi
 
 #
 # Сжать файл file.ext в /tmp/file.ext.gz с сохранением (-k) оригинального файла:
@@ -54,4 +81,3 @@ for activevm in $vm_list
 #
 # Распаковать (-d) файл file.ext.gz в файл /tmp/file.ext с сохранением (-k) архива:
 # pigz -k -c -d file.ext.gz >/tmp/file.ext
-#
