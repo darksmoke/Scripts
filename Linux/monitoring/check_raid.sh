b06e471d1c8cd06c215cf813de7cdd9c2a761355
darksmoke <dsmoke@ukr.net>
2025-06-19 22:59:07 +0300

    Update check_raid.sh

--- Linux/monitoring/check_raid.sh
+++ Linux/monitoring/check_raid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-CONFIG_FILE="config.ini"
-source send_telegram.sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+source "$SCRIPT_DIR/send_telegram.sh"
+source "$SCRIPT_DIR/config.ini"
 
 if command -v mdadm &> /dev/null; then
   STATUS=$(cat /proc/mdstat | grep -E '\[.*U.*\]' || true)
