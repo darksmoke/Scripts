cf782cb9dbe92910cafd9d608386694c7a775e62
darksmoke <dsmoke@ukr.net>
2025-11-19 23:39:13 +0200

    release 2.1

--- Linux/monitoring/check_ram.sh
+++ Linux/monitoring/check_ram.sh
@@ -7,32 +7,30 @@ source "${SCRIPT_DIR}/utils.sh"
 source "${SCRIPT_DIR}/config.sh"
 
 check_dependency "free"
-check_dependency "awk"
 
 HOST=$(hostname)
 
-# Получаем Total и Available
 read -r TOTAL_MB AVAILABLE_MB <<< $(free -m | awk '/^Mem:/ {print $2, $7}')
 
 if [[ "$TOTAL_MB" -eq 0 ]]; then
-    log_msg "ERROR: RAM detection failed (Total is 0)"
+    log_msg "ERROR: RAM detection failed"
     exit 1
 fi
 
-# Расчет процента доступной памяти
 PERCENT_AVAILABLE=$(( 100 * AVAILABLE_MB / TOTAL_MB ))
+ALERT_ID="ram_low_memory"
 
 if (( PERCENT_AVAILABLE < RAM_THRESHOLD )); then
     MSG=$(cat <<EOF
-🧠 *Low Memory Alert: ${HOST}*
-
-📉 Free RAM: ${PERCENT_AVAILABLE}% (${AVAILABLE_MB}MB)
-💾 Total RAM: ${TOTAL_MB}MB
-⛔ Threshold: < ${RAM_THRESHOLD}%
+🧠 *Мало памяти (RAM): ${HOST}*
+📉 Свободно: ${PERCENT_AVAILABLE}% (${AVAILABLE_MB}MB)
+💾 Всего: ${TOTAL_MB}MB
+⛔ Порог: < ${RAM_THRESHOLD}%
 EOF
 )
-    send_telegram "$MSG"
-    log_msg "ALERT: Low RAM (${PERCENT_AVAILABLE}% free)"
+    manage_alert "$ALERT_ID" "ERROR" "$MSG"
+else
+    manage_alert "$ALERT_ID" "OK" ""
 fi
 
 exit 0
