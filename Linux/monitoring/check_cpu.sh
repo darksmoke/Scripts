584b9ca083d40f4b6be775541b01ec6582cd883c
darksmoke <dsmoke@ukr.net>
2025-11-19 23:38:48 +0200

    release 2.1

--- Linux/monitoring/check_cpu.sh
+++ Linux/monitoring/check_cpu.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 # /opt/monitoring/check_cpu.sh
+set -uo pipefail
 
-# Установка путей
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 source "${SCRIPT_DIR}/utils.sh"
 source "${SCRIPT_DIR}/config.sh"
 
-# Проверка зависимости
 check_dependency "bc"
 
-# Логика
+HOST=$(hostname)
 CORES=$(nproc)
 LOAD_AVG=$(awk '{print $1}' /proc/loadavg)
-# Расчет порога: (Ядра * Порог%) / 100
+# Расчет порога
 THRESHOLD_VAL=$(echo "scale=2; ${CORES} * ${CPU_THRESHOLD} / 100" | bc)
 
 # Сравнение
 IS_OVERLOADED=$(echo "${LOAD_AVG} > ${THRESHOLD_VAL}" | bc)
 
-if [[ "$IS_OVERLOADED" -eq 1 ]]; then
-    HOST=$(hostname)
-    MSG=$(cat <<EOF
-🔥 *High CPU Load: ${HOST}*
+ALERT_ID="cpu_high_load"
 
-⚙️ Cores: ${CORES}
+if [[ "$IS_OVERLOADED" -eq 1 ]]; then
+    MSG=$(cat <<EOF
+🔥 *Высокая нагрузка CPU: ${HOST}*
+⚙️ Ядер: ${CORES}
 📈 Load Avg (1m): \`${LOAD_AVG}\`
-⛔ Threshold: \`${THRESHOLD_VAL}\` (> ${CPU_THRESHOLD}%)
+⛔ Порог: \`${THRESHOLD_VAL}\` (> ${CPU_THRESHOLD}%)
 EOF
 )
-    send_telegram "$MSG"
-    log_msg "ALERT: CPU Load High ($LOAD_AVG)"
+    manage_alert "$ALERT_ID" "ERROR" "$MSG"
+else
+    manage_alert "$ALERT_ID" "OK" ""
 fi
 
 exit 0
